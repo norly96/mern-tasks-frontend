34def258b28a909470d34ff5285f5ba7af15a6c9
Norluis Galvez <norly021196@gmail.com>
2024-07-20 04:53:43 -0400

    add page profile

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,87 @@
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
-};
+import {
+  Heading,
+  Avatar,
+  Box,
+  Center,
+  Text,
+  Stack,
+  Button,
+  Link,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useAuth } from "../context/AuthContext";
 
-export default ProfilePage;
+export default function ProfilePage() {
+  const { user } = useAuth();
+  return (
+    <Center py={6}>
+      <Box
+        maxW={"320px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        p={6}
+        textAlign={"center"}
+      >
+        <Avatar
+          size={"xl"}
+          bg="gray.400"
+          src={"https://bit.ly/broken-link"}
+          mb={4}
+          pos={"relative"}
+          _after={{
+            content: '""',
+            w: 4,
+            h: 4,
+            bg: "green.300",
+            border: "2px solid white",
+            rounded: "full",
+            pos: "absolute",
+            bottom: 0,
+            right: 3,
+          }}
+        />
+        <Heading fontSize={"2xl"} fontFamily={"body"}>
+          {user?.username}
+        </Heading>
+        <Text fontWeight={600} color={"gray.500"} mb={4}>
+          {user?.email}
+        </Text>
+        <Stack mt={8} direction={"row"} spacing={4}>
+          <Button
+            flex={1}
+            fontSize={"sm"}
+            rounded={"full"}
+            bg={"green.400"}
+            color={"white"}
+            _hover={{
+              bg: "green.500",
+            }}
+            _focus={{
+              bg: "green.500",
+            }}
+          >
+            <Link href="/">To Home</Link>
+          </Button>
+          <Button
+            flex={1}
+            fontSize={"sm"}
+            rounded={"full"}
+            bg={"green.400"}
+            color={"white"}
+            _hover={{
+              bg: "green.500",
+            }}
+            _focus={{
+              bg: "green.500",
+            }}
+          >
+            {" "}
+            <Link href="/tasks">To Tasks</Link>
+          </Button>
+        </Stack>
+      </Box>
+    </Center>
+  );
+}
